172e81d6c756922b041e62175dadb198aa029209
Harrison Cooley <harrison_v_cooley@me.com>
2023-09-28 21:48:42 -0400

    Initial Commit

---
+++ src/main/java/com/cooley/soccerwebapp/SoccerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.cooley.soccerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooley/soccerwebapp/SoccerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooley.soccerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
